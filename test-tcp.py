33cc018c093956fc0d968ee9d09836226a5fbd9b
vapvin <tpehdj12@daum.net>
2020-04-01 18:20:33 +0900

    Pushing!

--- test-tcp.py
+++ test-tcp.py
@@ -1,7 +1,7 @@
 # test in vmware server tcp
 
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import socket
 import threading
@@ -16,3 +16,22 @@ server.bind(in_ip, in_port)
 server.listen(5)
 
 print "Listening on Port %d, ip: %s" % (in_port, in_ip)
+
+
+def handle_client(client_socket):
+
+    request = client_socket.recv(1024)
+    print "[*] Received %s" % request
+
+    client_socket.send("ACK!")
+
+    client_socket.close()
+
+
+while True:
+    client, addr = server.accept()
+
+    print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
+
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
